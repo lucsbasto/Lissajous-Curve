85a207e3898e22fb26f45ad4325b0b2edc617401
Lucas Bastos <lucsbasto@gmail.com>
2020-03-07 11:39:13 -0300

    codificando os circulos nas linhas

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ w = 40
 function setup() {
   createCanvas(400, 400);
   cols = width / w - 1;
+  rows = height / w - 1;
 }
 
 function draw() {
@@ -27,5 +28,22 @@ function draw() {
     strokeWeight(1)
     line(cx + x, 0, cx + x, height)
     }
+    stroke(1);
+    noFill();
+    for (let i = 0; i < rows; i++) {
+      cy = w + i * w + w / 2
+      cx = w / 2
+      stroke(1);
+      strokeWeight(1)
+      ellipse(cx, cy, d, d)
+      let x = r * cos(angle * (i + 1) - HALF_PI);
+      let y = r * sin(angle * (i + 1) - HALF_PI);
+      strokeWeight(4)
+      stroke(0)
+      point(cx+ x, cy+ y)
+      stroke(0, 50)
+      strokeWeight(1)
+      line(0, cy + y, width, cy + y)
+      }
     angle += 0.01
 }
